c0ba0a5700a1e5365d31552ace41ad5bbd0a8367
mrnazu <samuelamsalu1998@gmail.com>
2023-12-07 09:34:17 +0300

    0x01. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,19 +6,15 @@ def canUnlockAll(boxes):
     :return: True if all boxes can be opened, else return False.
     """
 
-    # Set to keep track of opened boxes
     open_boxes = {0}
 
-    # List to keep track of keys
     keys = boxes[0]
 
-    # Iterate through the keys and update the open boxes
     while keys:
         key = keys.pop()
         if 0 <= key < len(boxes) and key not in open_boxes:
             open_boxes.add(key)
             keys.extend(boxes[key])
 
-    # Check if all boxes are opened
     return len(open_boxes) == len(boxes)
 
